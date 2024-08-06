8e7a3905f3b32e9f0b6532907f3c1ef9bd6e7bae
Electrifier <samchijioke16@gmail.com>
2024-08-06 11:34:31 +0100

    Updated source file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import { createApp } from "@deroll/app";
 import { createWallet } from "@deroll/wallet";
 import dotenv from "dotenv";
+import { hexToString } from "viem";
 
-import "./database/config";
+import { configMongo } from "./database/config";
 import {
     contributeToCampaign, createCampaign,
-    parsePayload
+    getAmountRaised, getCampaign,
+    listContributors, parsePayloadJSON
 } from "./helpers";
 
 
 dotenv.config();
+configMongo();
 const wallet = createWallet();
 
 const app = createApp({
@@ -19,7 +22,7 @@ const app = createApp({
 app.addAdvanceHandler(wallet.handler);
 
 app.addAdvanceHandler(async ({ metadata, payload }) => {
-    const jsonPayload = parsePayload(payload);
+    const jsonPayload = parsePayloadJSON(payload);
     if (jsonPayload == null) {
         return "reject";
     }
@@ -30,4 +33,45 @@ app.addAdvanceHandler(async ({ metadata, payload }) => {
         contributeToCampaign({ metadaata, payload: jsonPayload });
     }
 
+    return "accept";
+});
+
+app.addInspectHandler(async ({ metadata, payload }) => {
+    const path = hexToString(payload).split("/") // inspect/path/to/resource
+
+    switch (path[1]) {
+        case "listContributors": // inspect/0x`${String}`/listContributors
+            const contributors = await listContributors(metadata, path);
+            sendReport({ payload: contributors });
+            break;
+
+        case "amountRaised": // inspect/0x`${String}`/amount_raised
+            const amountRaised = await getAmountRaised(path[0]);
+            if (!amountRaised) {
+                sendReport({ error: "No campaign with this id exists." });
+                return "reject";
+            }
+            sendReport({payload: amountRaised});
+            break;
+
+        case "about": // inspect/0x`${String}`/about
+            const campaignObj = await getCampaign(path[0]);
+			if (!campaignObj) {
+				sendReport({ error: "No campaign with this id exists." });
+                return "reject";
+			}
+            sendReport({ payload: campaignObj });
+            break;
+
+        default:
+            break;
+    }
+
+    return "accept";
+})
+
+// Start the application
+app.start().catch((e) => {
+    console.error(e);
+    process.exit(1);
 });
